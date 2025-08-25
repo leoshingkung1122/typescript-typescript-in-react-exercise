846a5ab89abb6d0f5e300830671d956df5aa7bee
PHET45 <phnjk2000@gmail.com>
2025-08-25 13:29:25 +0700

    Implement Greeting component in App and add TypeScript types for props and state

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import "./App.css";
+import Greeting from "./Greeting";
 
 function App() {
   // TODO4 : Render ออกมาใน App.tsx
-  return;
+  return (
+    <div>
+      <Greeting name="John" age={25} />
+    </div>
+  );
 }
 
 export default App;
--- src/Greeting.tsx
+++ src/Greeting.tsx
@@ -2,16 +2,24 @@
 import { useState } from "react";
 
 // TODO1:  ประกาศ Type ด้วย type หรือ inerface
+interface GreetingProps {
+  name: string;
+  age: number;
+}
+
+type ID = string | number;
 
 // TODO2: กำหนด Type ให้ props
-const Greeting = (props) => {
+const Greeting = (props: GreetingProps) => {
   // TODO3: กำหนด Type ให้ useState
-  const [count, setCount] = useState();
-
+  const [count, setCount] = useState<number>(0);
+  const id: ID = 123;
+  
   return (
     <div>
       <h1>
         Hello {props.name}, you are {props.age} years old 👋
+        {id}
       </h1>
       <p>You clicked {count} times</p>
       <button onClick={() => setCount(count + 1)}>Click Me</button>
